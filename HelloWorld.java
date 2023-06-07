0331951040ca8d2fd3f1394c0422269d5eb3b1b0
kieran <kieran.riescrossley@hotmail.com>
2023-06-07 10:52:44 +0200

    feature A - ein bisschen aufräumen

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,25 +1,20 @@
 public class HelloWorld {
     private int i;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     private String name;
     
->>>>>>> bc366a67dd4f9b4d05d2b58e483cc88367dca2da
-    public HelloWorld() {
-        i = 0;
-    }
-=======
+
     private String text;
+    
     public HelloWorld() {
         i = 0;
         text = "";
     }
+    
     public HelloWorld(String text) {
+    	this.text = text;
         System.out.println(text);
     }
 
->>>>>>> featureA
     public int getI() {
         return i;
     }
