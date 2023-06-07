38171361027cc4ba72774a84d01040c46bfe0c45
kieran <kieran.riescrossley@hotmail.com>
2023-06-07 10:26:39 +0200

    feature B - new getter/setter

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,19 @@
 public class HelloWorld {
     private int i;
+    private String name;
+    
     public HelloWorld() {
         i = 0;
     }
     public int getI() {
         return i;
+    }
+    
+    public String getName(){
+    	return name;
+    }
+    
+    public void setName(String name){
+    	this.name = name;
     }
 }
