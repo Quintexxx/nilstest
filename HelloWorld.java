8247b675110f36a48411d44cd2fea7f95c7ec7c9
Quintexxx <martin.brunner06@gmail.com>
2023-06-07 10:26:19 +0200

    feature A - new constructor

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,14 @@
 public class HelloWorld {
     private int i;
+    private String text;
     public HelloWorld() {
         i = 0;
+        text = "";
     }
+    public HelloWorld(String text) {
+        System.out.println(text);
+    }
+
     public int getI() {
         return i;
     }
